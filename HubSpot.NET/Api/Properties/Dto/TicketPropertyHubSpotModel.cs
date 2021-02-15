f4c33f3cc3b86912efbafdf49008c85b9e81a51a
Kyle <Kelly>
2021-02-15 17:12:15 -0500

    Feature - Add Ticket Model

---
+++ HubSpot.NET/Api/Properties/Dto/TicketPropertyHubSpotModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HubSpot.NET.Core.Interfaces;
+using System.Runtime.Serialization;
+
+namespace HubSpot.NET.Api.Properties.Dto
+{
+    public class TicketPropertyHubSpotModel : IHubSpotModel
+    {
+        [DataMember(Name = "hs_lastmodifieddate")]
+        public DateTime hs_lastmodifieddate { get; set; }
+
+        [DataMember(Name = "hs_pipeline")]
+        public string hs_pipeline { get; set; }
+
+        [DataMember(Name = "hs_pipeline_stage")]
+        public string hs_pipeline_stage { get; set; }
+
+        [DataMember(Name = "hs_ticket_priority")]
+        public string hs_ticket_priority { get; set; }
+
+        [DataMember(Name = "hubspot_owner_id")]
+        public string hubspot_owner_id { get; set; }
+
+        [DataMember(Name = "subject")]
+        public string subject { get; set; }
+
+        public bool IsNameValue => false;
+
+    }
+}
---
+++ HubSpot.NET/Api/Ticket/Dto/TicketHubSpotModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using HubSpot.NET.Core.Interfaces;
+
+namespace HubSpot.NET.Api.Ticket.Dto
+{
+
+    /// <summary>
+    /// Models the associations of a deal to companies & contacts
+    /// </summary>
+    [DataContract]
+    public class TicketHubSpotAssociations
+    {
+        [DataMember(Name = "associatedCompanyIds")]
+        public long[] AssociatedCompany { get; set; }
+
+        [DataMember(Name = "associatedVids")]
+        public long[] AssociatedContacts { get; set; }
+    }
+
+    /// <summary>
+    /// Models a Ticket entity within HubSpot. Default properties are included here
+    /// with the intention that you'd extend this class with properties specific to 
+    /// your HubSpot account.
+    /// </summary>
+    [DataContract]
+    public class TicketHubSpotModel : IHubSpotSerializable<TicketHubSpotModel>
+    {
+        public TicketHubSpotModel()
+        {
+
+        }
+
+        [DataMember(Name = "archived")]
+        public bool archived { get; set; }
+        public string id { get; set; }
+        public bool IsNameValue => true;
+
+        [IgnoreDataMember]
+        public TicketHubSpotAssociations Associations { get; set; }
+
+        public virtual void ToHubSpotDataEntity(ref TicketHubSpotModel converted)
+        {
+            converted.Associations = Associations;
+        }
+
+        public virtual void FromHubSpotDataEntity(TicketHubSpotModel hubspotData)
+        {
+            if (hubspotData.Associations != null)
+            {
+                Associations.AssociatedContacts = hubspotData.Associations.AssociatedContacts;
+                Associations.AssociatedCompany = hubspotData.Associations.AssociatedCompany;
+            }
+        }
+    }
+}
